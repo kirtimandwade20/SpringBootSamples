605c70744af1081c293daa47004418d92b27eb2f
kirti.mandwade.neosoft <kirti.mandwade.neosoft@hopscotch.in>
2022-06-30 18:42:12 +0530

    Sample application with drools rule engine for 2 or more DRL File

---
+++ Drools-Sample-DRLFile/Readme.md
@@ -0,0 +1,77 @@
+# Drools Sample with dynamic loading of multiple DRL files
+
+This is a sample application to create a rule engine using drools. 
+## Use case
+Having 2 or more DRL files and having the ability to fire rules of any DRL file on the basis of
+API Call. In the sample i have used 2 user's (static user's for now) and we can fire rules for different user
+on the basis of the received request
+We can also dynamically update the rules using reload API.
+
+
+
+## Running the application 
+You can directly start the application by executing the DroolsDemoApplication.class or
+
+
+``` Java -jar drools-demo-0.0.1-SNAPSHOT.jar
+
+```
+
+## Testing
+
+Executing rules for "Tenant 1" 
+```
+Method POST
+http://localhost:8080/fireRules?tenant=tenant1
+Request Body:
+{
+ "bankName": "Citi",
+ "durationInYear": 1
+}
+Result
+
+```
+{
+    "fields": {
+        "bankName": "Citi",
+        "durationInYear": 1
+    },
+    "results": {
+        "rate": "2%"
+    }
+}
+```
+
+```
+Executing rules for "Tenant 2" 
+```
+Method POST
+http://localhost:8080/fireRules?tenant=tenant2
+Request Body:
+{
+ "age": 3
+}
+
+```
+Result
+```
+{
+    "fields": {
+        "age": 3
+    },
+    "results": {
+        "type": "child"
+    }
+}
+```
+
+Reload Configuraion
+You can update the configuration by making change in DRL file and then executing following API
+```
+Method GET
+http://localhost:8080/reload?tenant=tenant2
+```
+Result
+```
+config for tenant2 updated
+```
---
+++ Drools-Sample-DRLFile/src/main/java/com/drool/example/config/DroolsConfiguration.java
@@ -0,0 +1,64 @@
+package com.drool.example.config;
+
+import java.util.HashMap;
+
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.drools.core.impl.KnowledgeBaseFactory;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieModule;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieContainer;
+import org.kie.internal.builder.KnowledgeBuilder;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.io.ResourceFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DroolsConfiguration {
+
+	private final KieServices kieServices = KieServices.Factory.get();
+	public static HashMap<String, InternalKnowledgeBase> hmKnowledgebase = new HashMap();
+
+	public static HashMap<String, String> hmDrlFileNames = new HashMap();
+
+	static {
+		hmDrlFileNames.put("tenant1", "FDInterestRate.drl");
+		hmDrlFileNames.put("tenant2", "AgeFinderRule.drl");
+
+	}
+	
+
+	public static KnowledgeBuilder knowledgeBuilder(String filename) throws Exception {
+		KnowledgeBuilder builder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		builder.add(ResourceFactory.newClassPathResource(filename), ResourceType.DRL);
+		if (builder.hasErrors()) {
+			throw new Exception(String.valueOf(builder.getErrors()));
+		}
+		return builder;
+	}
+
+	@Bean
+	public void knowledgeBase() throws Exception {
+		InternalKnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
+		knowledgeBase.addPackages(knowledgeBuilder("FDInterestRate.drl").getKnowledgePackages());
+		hmKnowledgebase.put("tenant1", knowledgeBase);
+		InternalKnowledgeBase knowledgeBase2 = KnowledgeBaseFactory.newKnowledgeBase();
+
+		knowledgeBase2.addPackages(knowledgeBuilder("AgeFinderRule.drl").getKnowledgePackages());
+		hmKnowledgebase.put("tenant2", knowledgeBase2);
+
+	}
+
+	public static void reloadKnowledgeBase(String tenant) throws Exception {
+		hmKnowledgebase.get(tenant);
+		InternalKnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
+		knowledgeBase.addPackages(knowledgeBuilder(hmDrlFileNames.get(tenant)).getKnowledgePackages());
+		hmKnowledgebase.put(tenant, knowledgeBase);
+
+	}
+
+}
